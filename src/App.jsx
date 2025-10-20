95f6e91a68e5801ca51c201af65f256002a7e618
anyasristy <148508255+anyasristy@users.noreply.github.com>
2025-10-20 13:11:01 -0400

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import {useState} from "react";
-import guest_details from "./guests/guest_details";
-import guest_list from "./guests/guest_list";
+import { useState } from "react";
+import Guest_details from "./guests/guest_details";
+import Guest_list from "./guests/guest_list";
 export default function App() {
   const [guestId, set_ID] = useState(null);
   return (
     <main>
       {guestId ? (
-        <guest_details guestId={guestId}set_ID={set_ID} />
+        <Guest_details guestId={guestId} set_ID={set_ID} />
       ) : (
-        <guest_list set_ID={set_ID} />
+        <Guest_list set_ID={set_ID} />
       )}
     </main>
   );

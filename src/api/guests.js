f2ab299bf63c1d4250d696e6b61d427af5bc1458
anyasristy <148508255+anyasristy@users.noreply.github.com>
2025-10-20 13:08:33 -0400

    commit

--- src/api/guests.js
+++ src/api/guests.js
@@ -3,16 +3,17 @@ export async function getting_guest() {
   try {
     const response = await fetch(API + "/guests");
     const result = await response.json();
-    return result.data;
+    return result.data.guests; 
   } catch (e) {
     console.error(e);
-    return [];}
+    return [];
+  }
 }
 export async function getGuest(id) {
   try {
-    const response = await fetch(API+"/guests/"+id);
-    const result =await response.json();
-    return result.data;
+    const response = await fetch(API + "/guests/" + id);
+    const result = await response.json();
+    return result.data.guest; 
   } catch (e) {
     console.error(e);
     return null;
